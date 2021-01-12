ef174686657bccb9eb657ecd32a1fa3404d323ea
gerardmi24 <gerardmistretta@yahoo.com>
2021-01-12 16:24:47 -0500

    commit tree 07ed3ff216cf68f1bd7f24b7b8604e8c96ddc100

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+export default function EyesOnMe() {
+
+    const blurry = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    const zonedIn = () => {
+    console.log('Good!')
+    }
+
+    return (
+        <button onFocus={zonedIn} onBlur={blurry} text="Eyes on me">
+            Eyes on me
+        </button>
+    )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+import React from 'react'
+
+export default function Keypad() {
+
+    const eventHandler = () => {
+        console.log('Entering password...')
+    }
+    
+    return (
+        <input onChange={eventHandler} type="password" />
+    )
+}
